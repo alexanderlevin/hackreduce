9203d1eddffde2457243366e3d628b0a7db4bea6
wpli <valleygreen@kirchweg.com>
2013-05-21 15:28:14 -0400

    all of the features that we used

--- matlab_src/extract_features.m
+++ matlab_src/extract_features.m
@@ -1,4 +1,7 @@
 function [ x, y ] = extract_features( data )
+% extract_features( data    Extract features from the raw data.
+%                           The features we used are described and
+%                           extracted here.
 
 n = size(data, 1);
 
@@ -6,15 +9,31 @@ x = [];
 y = zeros(n, 1);
 
 for i = 1:n
-    p = data{i}.X;
+    p = data{i};
     
-    x_row = [p(1:13, 4)./p(1:13, 3)*p(26, 3); p(1:13, 4); p(:, 3)];
+    % 13 features: sales(week_i) / stores( week_i ) * stores( week_26 ) for
+    % weeks i=1 to i=13
+    x_row = [p.X(1:13, 4)./p.X(1:13, 3)*p.X(26, 3); p.X(1:13, 4)];
+    
+    % 13 features: raw sales values between weeks 1 and 13
+    x_row = [ x_row; p.X(1:13,4) ];
+    
+    % 13 features: number of stores between weeks 14 and 26    
+    % add stores between weeks 14 and 26
+    x_row = [ x_row; p.X(14:26,3) ];
+    
+    % add some interaction terms
+    % 3 features: interaction terms
+    finalstores_midsales = exp( log(p.X(26,3) + 1 ) * log(p.X(13,4) + 1) );
+    midz_midsales = exp( ( log( p.X(13,4) / p.X(13,3) * p.X( 26,3) + 1 )  ) * ( log(p.X(13,4) + 1 )  ) );
+    finalscores_midz  = exp( ( log( p.X(13,4) / p.X(13,3) * p.X(26,3) +1) ) * log(p.X(26,3)+1) );
+    x_row = [ x_row; finalstores_midsales; midz_midsales; finalscores_midz ];
+    
     x = [x; x_row'];
     
-    y(i) = p(26, 4);
+    y(i) = p.y;
 end
 
-x = log(x + 1);
-x = [x, x(:,13) .* x(:, 26), x(:, 26) .* x(:, 52), x(:, 13) .* x(:, 52)];
+
 end
 
